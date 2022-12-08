eac63ddb4be2b07af91ce60ed58c92adae631a78
mpiastucki <michael.piastucki@woven-planet.global>
2022-12-08 10:36:10 +0900

    fix: change build output folder

--- server/index.js
+++ server/index.js
@@ -4,10 +4,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 9000;
 
-app.use(express.static(path.resolve(__dirname, '../build')));
+app.use(express.static(path.resolve(__dirname, '../dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../dist', 'index.html'));
   });
 
 app.listen(port, () => {
